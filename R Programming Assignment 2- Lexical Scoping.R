75daf0baf4ebfdeb056c30808e41e46f6d081fa4
CancerCarol97 <51211021+CancerCarol97@users.noreply.github.com>
2019-06-17 16:41:11 -0500

    Update R Programming Assignment 2- Lexical Scoping.R

--- R Programming Assignment 2- Lexical Scoping.R
+++ R Programming Assignment 2- Lexical Scoping.R
@@ -22,6 +22,8 @@ makeVector <- function(x = numeric()) {
 				x$setmean(m)
 				m
 				}
+# A pair of functions that cache the inverse of a matrix.
+# This function creates a special “matrix” object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
 	set <- function(y) {
@@ -33,7 +35,9 @@ makeCacheMatrix <- function(x = matrix()) {
 		getInverse <- function() inv
 		list (set = set, get= get, setInverse = setInverse, getInverse = getInverse)
 		}
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 		cacheSolve <- function(x, ...) {
+		# Return a matrix that is the inverse of 'x'. 
 			inv <- x$getInverse()
 			if(!is.null(inv)) {
 				message("getting cached data")
